eeafe2ddf7eaf03425dab867d41113dd79a73343
cjmcguire88 <cjmcguire88@gmail.com>
2022-01-06 17:28:13 -0500

    added case to print sink if ''=sink

--- polybar/.config/polybar/scripts/sound.sh
+++ polybar/.config/polybar/scripts/sound.sh
@@ -4,7 +4,9 @@
 main() {
     SINK=$(pw-play --list-targets | sed -n 's/^*.*"\(.*\)" prio=.*$/\1/p')
     VOLUME=$(pamixer --get-volume-human)
+    [[ $1 =~ sink ]] && echo $SINK
     [[ ! $VOLUME =~ "muted" ]] && echo " ${VOLUME}" || echo "  "
+
 }
 
 main $@
