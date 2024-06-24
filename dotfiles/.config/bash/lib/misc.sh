b9ca403cf1e684d781894651d756f38965adcb46
Charles McGuire <cjmcguire88@gmail.com>
2024-06-23 21:38:46 -0400

    reboot

--- dotfiles/.config/bash/lib/misc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# only define LC_CTYPE if undefined
-if [[ -z "$LC_CTYPE" && -z "$LC_ALL" ]]; then
-  export LC_CTYPE=${LANG%%:*} # pick the first entry from LANG
-fi
-
-# recognize comments
-shopt -s interactive_comments
--- dotfiles/.config/bash/lib/theme-and-appearance.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# colored ls
-export LSCOLORS='Gxfxcxdxdxegedabagacad'
